f724f870c42bcc4fcde4bc2d60bc20bcf7431f74
Korry Tunnicliff <korry.tunnicliff@colorado.edu>
2023-04-14 17:11:12 -0600

    chore: Image backgrounds to popular hero carousel classes

--- FuffRN/components/HeroCarousel.tsx
+++ FuffRN/components/HeroCarousel.tsx
@@ -1,6 +1,18 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+} from 'react-native';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
+
+const yogaImg = require('./assets/yoga.jpg');
+const zumbaImg = require('./assets/zumba.jpg');
+const pilatesImg = require('./assets/pilates.jpg');
+const spinningImg = require('./assets/spinning.jpg');
+const kickboxingImg = require('./assets/kickboxing.jpg');
 
 interface CarouselItem {
   title: string;
@@ -15,19 +27,23 @@ const carouselHeight = screenHeight * 0.17;
 
 const HeroCarousel: React.FC<HeroCarouselProps> = ({
   items = [
-    {title: 'Yoga'},
-    {title: 'Zumba'},
-    {title: 'Pilates'},
-    {title: 'Spinning'},
-    {title: 'Kickboxing'},
+    {title: 'Yoga', image: yogaImg},
+    {title: 'Zumba', image: zumbaImg},
+    {title: 'Pilates', image: pilatesImg},
+    {title: 'Spinning', image: spinningImg},
+    {title: 'Kickboxing', image: kickboxingImg},
   ],
 }) => {
   const [activeSlide, setActiveSlide] = useState(0);
   const renderItem = ({item, index}: {item: CarouselItem; index: number}) => {
     return (
-      <View style={styles.carouselItem}>
+      <ImageBackground
+        source={item.image}
+        style={styles.carouselItem}
+        imageStyle={{borderRadius: 5}}>
+        <View style={styles.overlay} />
         <Text style={styles.carouselTitle}>{item.title}</Text>
-      </View>
+      </ImageBackground>
     );
   };
 
@@ -81,6 +97,15 @@ const styles = StyleSheet.create({
   },
   paginationInactiveDot: {
     backgroundColor: '#A2A4A3',
+  },
+  overlay: {
+    backgroundColor: 'rgba(207, 184, 124, 0.95)',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    borderRadius: 5,
   },
 });
 
