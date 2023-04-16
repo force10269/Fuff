78566f5a2f27f0aa7a055c9d1dc5c8147512c67f
Korry Tunnicliff <korry.tunnicliff@colorado.edu>
2023-04-16 12:33:42 -0600

    feat: Member ID label for QR code

--- FuffRN/components/QRCodeButton.tsx
+++ FuffRN/components/QRCodeButton.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {TouchableOpacity, Image, StyleSheet} from 'react-native';
+import {TouchableOpacity, Image, StyleSheet, Text, View} from 'react-native';
 import {useNavigation, useRoute} from '@react-navigation/native';
 
 interface QRCodeButtonProps {
@@ -29,10 +29,13 @@ const QRCodeButton: React.FC<QRCodeButtonProps> = ({
     <TouchableOpacity
       onPress={() => navigate('QR Code Overlay')}
       style={[styles(onHomePage).qrButton, {zIndex}]}>
-      <Image
-        source={require('./assets/qr-code-icon.png')}
-        style={styles().qrIcon}
-      />
+      <View style={styles().qrContainer}>
+        <Image
+          source={require('./assets/qr-code-icon.png')}
+          style={styles().qrIcon}
+        />
+      </View>
+      <Text style={styles().qrLabel}>Member ID</Text>
     </TouchableOpacity>
   );
 };
@@ -41,20 +44,30 @@ const styles = (onHomePage: Boolean = false) =>
   StyleSheet.create({
     qrButton: {
       position: 'absolute',
-      bottom: onHomePage ? 40 : 80,
+      bottom: onHomePage ? 30 : 80,
       right: 20,
-      width: 70,
-      height: 70,
-      borderRadius: 50,
+      width: 100,
+      height: 100,
+      borderRadius: 35,
       backgroundColor: '#0275d8',
       justifyContent: 'center',
       alignItems: 'center',
       zIndex: 1,
     },
     qrIcon: {
-      width: 40,
-      height: 40,
+      width: 50,
+      height: 50,
       tintColor: 'black',
+    },
+    qrContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    qrLabel: {
+      marginTop: 4,
+      color: 'black',
+      fontSize: 12,
+      fontWeight: 'bold',
     },
   });
 
