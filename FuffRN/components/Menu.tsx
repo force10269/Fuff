44ee8f5bd44735f503c3dff7457898724411745f
Korry Tunnicliff <korry.tunnicliff@colorado.edu>
2023-04-18 13:48:37 -0600

    feat: Icons in Menu component

--- FuffRN/components/Menu.tsx
+++ FuffRN/components/Menu.tsx
@@ -26,12 +26,13 @@ const Menu = (): JSX.Element => {
     menuItems.map((item, index) => (
       <TouchableOpacity
         key={index}
-        style={styles.menuItem}
+        style={styles(isMenuOpen).menuItem}
         onPress={() => {
           handleMenuPress();
           navigate(`${item.screen}`);
         }}>
-        <Text style={styles.menuItemTitle}>{item.title}</Text>
+        <Image source={item.icon} style={styles(isMenuOpen).menuItemIcon} />
+        <Text style={styles(isMenuOpen).menuItemTitle}>{item.title}</Text>
       </TouchableOpacity>
     ));
 
@@ -39,84 +40,94 @@ const Menu = (): JSX.Element => {
     <>
       {isMenuOpen && (
         <View
-          style={styles.overlay}
+          style={styles(isMenuOpen).overlay}
           onStartShouldSetResponder={() => true}
           onResponderRelease={handleMenuPress}
         />
       )}
-      <View style={styles.menuContainer}>
+      <View style={styles(isMenuOpen).menuContainer}>
         <QRCodeButton zIndex={isMenuOpen ? -1 : 1} hideOnQRCodeOverlay />
         <View>
-          <View style={styles.halfOval}>
+          <View style={styles(isMenuOpen).halfOval}>
             <TouchableOpacity
               onPress={handleMenuPress}
-              style={styles.menuButton}>
-              <Image source={menuIcon} style={styles.menuIcon} />
+              style={styles(isMenuOpen).menuButton}>
+              <Image source={menuIcon} style={styles(isMenuOpen).menuIcon} />
             </TouchableOpacity>
           </View>
-          <View style={styles.bottomRectangle} />
+          <View style={styles(isMenuOpen).bottomRectangle} />
         </View>
         {isMenuOpen && (
-          <View style={styles.menuItemsContainer}>{renderMenuItems()}</View>
+          <View style={styles(isMenuOpen).menuItemsContainer}>{renderMenuItems()}</View>
         )}
       </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  menuContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    zIndex: 1,
-  },
-  bottomRectangle: {
-    width: '100%',
-    height: 20,
-    backgroundColor: '#CFB87C',
-  },
-  halfOval: {
-    width: '100%',
-    height: 50,
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-    borderColor: 'black',
-    backgroundColor: '#CFB87C',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  menuButton: {
-    alignItems: 'center',
-  },
-  menuIcon: {
-    width: 30,
-    height: 40,
-  },
-  menuItemsContainer: {
-    backgroundColor: '#CFB87C',
-    alignItems: 'center',
-    textAlign: 'center',
-    justifyContent: 'center',
-    paddingBottom: 40,
-  },
-  menuItem: {
-    textAlign: 'center',
-    padding: 15,
-  },
-  menuItemTitle: {
-    fontSize: 16,
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 1,
-  },
-});
+const styles = (isMenuOpen: Boolean) =>
+  StyleSheet.create({
+    menuContainer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      zIndex: 1,
+    },
+    bottomRectangle: {
+      width: '100%',
+      height: isMenuOpen ? 0 : 20,
+      backgroundColor: '#CFB87C',
+    },
+    halfOval: {
+      width: '100%',
+      height: 50,
+      borderTopLeftRadius: 40,
+      borderTopRightRadius: 40,
+      borderColor: 'black',
+      backgroundColor: '#CFB87C',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    menuButton: {
+      alignItems: 'center',
+    },
+    menuIcon: {
+      width: 50,
+      height: 50,
+    },
+    menuItemsContainer: {
+      backgroundColor: '#CFB87C',
+      alignItems: 'center',
+      textAlign: 'center',
+      justifyContent: 'center',
+      paddingBottom: 30,
+      display: isMenuOpen ? 'flex' : 'none',
+    },
+    menuItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      textAlign: 'center',
+      padding: 18,
+    },
+    menuItemIcon: {
+      width: 24,
+      height: 24,
+      marginRight: 8,
+    },
+    menuItemTitle: {
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      zIndex: 1,
+    },
+  });
 
 export default Menu;
--- FuffRN/components/data/MenuData.tsx
+++ FuffRN/components/data/MenuData.tsx
@@ -1,31 +1,46 @@
+const homeIcon = require('../assets/home-icon.png');
+const notificationsIcon = require('../assets/notifications-icon.png');
+const hoursIcon = require('../assets/hours-icon.png');
+const classesIcon = require('../assets/classes-icon.png');
+const headcountIcon = require('../assets/headcount-icon.png');
+const clubsIcon = require('../assets/clubs-icon.png');
+const passesIcon = require('../assets/passes-icon.png');
+
 const menuItems = [
   {
     title: 'Home',
     screen: 'Home Page',
+    icon: homeIcon,
   },
   {
     title: 'Notifications',
     screen: 'Notifications Page',
+    icon: notificationsIcon,
   },
   {
     title: 'Hours/Schedules',
     screen: 'Hours Schedules Page',
+    icon: hoursIcon,
   },
   {
     title: 'Classes',
     screen: 'Classes Page',
+    icon: classesIcon,
   },
   {
     title: 'Headcount',
     screen: 'Live Head Count Page',
+    icon: headcountIcon,
   },
   {
     title: 'Intramural Sports',
     screen: 'Intramurals/Clubs Page',
+    icon: clubsIcon,
   },
   {
     title: 'Semester Passes',
     screen: 'Semester Passes',
+    icon: passesIcon,
   },
 ];
 
