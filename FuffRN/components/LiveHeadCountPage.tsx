6b6eb2b8d4d85569555e6823e83a41425c37bc4d
Korry Tunnicliff <korry.tunnicliff@colorado.edu>
2023-04-23 00:04:00 -0600

    chore: Playing around with color values for progress bars

--- FuffRN/components/LiveHeadCountPage.tsx
+++ FuffRN/components/LiveHeadCountPage.tsx
@@ -4,28 +4,44 @@ import Menu from './Menu';
 import sampleData from './data/LiveHeadCountData';
 
 const LiveHeadCountPage = () => {
+  const shades = [
+    '#d1f3d3',
+    '#b6e5b5',
+    '#9bd692',
+    '#7fb96f',
+    '#649c4b',
+    '#48802a',
+    '#2b6621',
+    '#17732e',
+    '#da3e3e',
+    '#da1f1f',
+    '#da0c0c',
+  ];
+
   return (
     <View style={styles.container}>
       <Menu />
       <ScrollView>
         <View style={styles.content}>
           <Text style={styles.title}>Live Headcount</Text>
-          {sampleData.map(item => (
-            <View key={item.id} style={styles.card}>
-              <Text style={styles.cardTitle}>{item.title}</Text>
-              <View style={styles.progressBar}>
-                <View
-                  style={[
-                    styles.progressInner,
-                    item.progress <= 50 && styles.progressInnerGreen,
-                    item.progress > 50 && item.progress < 75 && styles.progressInnerYellow,
-                    item.progress >= 75 && styles.progressInnerRed,
-                    {width: `${item.progress}%`},
-                  ]}
-                />
+          {sampleData.map(item => {
+            const progress = Math.min(Math.round(item.progress / 10), shades.length - 1);
+            const progressColor = shades[progress];
+            return (
+              <View key={item.id} style={styles.card}>
+                <Text style={styles.cardTitle}>{item.title}</Text>
+                <View style={styles.progressBar}>
+                  <View
+                    style={[
+                      styles.progressInner,
+                      {backgroundColor: progressColor},
+                      {width: `${item.progress}%`},
+                    ]}
+                  />
+                </View>
               </View>
-            </View>
-          ))}
+            );
+          })}
         </View>
       </ScrollView>
     </View>
@@ -65,15 +81,6 @@ const styles = StyleSheet.create({
   },
   progressInner: {
     height: 20,
-  },
-  progressInnerGreen: {
-    backgroundColor: '#aadadc',
-  },
-  progressInnerYellow: {
-    backgroundColor: '#f1fa8c',
-  },
-  progressInnerRed: {
-    backgroundColor: '#e63946',
   },
 });
 
