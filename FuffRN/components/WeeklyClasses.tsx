529b0f157747095c2cdd8e9d71d9aa83de31bd3d
Korry Tunnicliff <korry.tunnicliff@colorado.edu>
2023-04-23 00:51:10 -0600

    fix: Padding at the bottom of Classes page

--- FuffRN/components/WeeklyClasses.tsx
+++ FuffRN/components/WeeklyClasses.tsx
@@ -1,61 +1,64 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { weeklyClassesData } from './data/WeeklyClassesData';
+import {View, Text, StyleSheet} from 'react-native';
+import {weeklyClassesData} from './data/WeeklyClassesData';
 
 const WeeklyClasses = () => {
     const daysOfWeek = ['M', 'T', 'W', 'Th', 'F'];
-  
+
     return (
-      <View style={styles.container}>
+        <View style={styles.container}>
         {daysOfWeek.map((day, index) => (
-          <View key={index} style={styles.dayContainer}>
+            <View key={index} style={styles.dayContainer}>
             <Text style={styles.dayLabel}>{day}</Text>
             {weeklyClassesData[day].map((classData, classIndex) => (
-              <View key={classIndex} style={styles.classItem}>
+                <View key={classIndex} style={styles.classItem}>
                 <Text style={styles.classTime}>{classData.time}</Text>
                 <Text style={styles.classTitle}>{classData.title}</Text>
                 <Text style={styles.classLocation}>{classData.location}</Text>
-              </View>
+                </View>
             ))}
-          </View>
+            </View>
         ))}
-      </View>
+        </View>
     );
-  };
+};
   
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    marginBottom: 20,
-  },
-  dayContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 5,
-  },
-  dayLabel: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  classItem: {
-    marginBottom: 5,
-  },
-  classTime: {
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-  classTitle: {
-    fontSize: 14,
-  },
-  classLocation: {
-    fontSize: 12,
-    fontStyle: 'italic',
-  },
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        marginBottom: 20,
+        marginHorizontal: -15,
+        paddingHorizontal: 5,
+    },
+    dayContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingHorizontal: 10,
+    },
+    dayLabel: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    classItem: {
+      marginBottom: 5,
+    },
+    classTime: {
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+    classTitle: {
+      fontSize: 14,
+    },
+    classLocation: {
+      fontSize: 12,
+      fontStyle: 'italic',
+    },
 });
+  
 
 export default WeeklyClasses;
